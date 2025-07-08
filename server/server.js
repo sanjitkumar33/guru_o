658bd6d965569fd59d8e7e6ba4f4bdbc869c6f00
sanjitkumar33 <kumarsan5233@gmail.com>
2025-07-08 13:03:05 +0530

    add code

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const authRoutes = require("./routes/auth");
-
-dotenv.config();
-
 const app = express();
-app.use(cors());
-app.use(express.json());
+require('dotenv').config();
+require('./Models/db');
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("DB Error:", err));
+const PORT = process.env.PORT || 7028;
 
-app.use("/api", authRoutes);
+app.get('/ping',(req,res) =>{
+  res.send('PONG');
+});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,() =>{
+    console.log(`server is running on ${PORT}`)
+});
+
+
+
+
