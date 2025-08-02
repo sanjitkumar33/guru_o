8dd6a3ff6ac5204cd2b2ff5c41fc6b0f3c95263c
sanjitkumar33 <kumarsan5233@gmail.com>
2025-08-02 09:29:36 +0530

    new backend code

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -2,5 +2,28 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.schema({
-    
+    name: String,
+    email: String,
+    password: {
+        type: String,
+        minLength: [8, "password must have at least 8 character."],
+        maxLength: [32, "password cannot have more than 32 character."],
+
+    },
+    phone: String,
+    accountVerified: {type: boolean, default: false},
+    verificationCode: Number,
+    verificationCodeExpire: Date,
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt:{
+        type: Date,
+        default: Date.now,
+    },
 })
+
+userSchema.pre("save", async function (next){
+    if (!this.isModified("password")){
+        next();
+    }
+});
